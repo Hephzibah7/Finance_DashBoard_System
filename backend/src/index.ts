4e59f6c0ee69a23ba9174eca7198099955510502
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-04-13 10:18:26 +0530

    refactor:added public access in cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(cors({
-  origin: FRONTEND_URL,
+  origin: "*",
   credentials: true,
 }));
 
